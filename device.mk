56a2afa103b40e7bb066e9213ae5ecb51c533790
ned <iamcalledned@gmail.com>
2012-12-10 11:36:34 -0500

    added bootanimation and overrides

--- device.mk
+++ device.mk
@@ -42,6 +42,11 @@ PRODUCT_COPY_FILES += \
     device/samsung/manta/fstab.smdk5250:root/fstab.smdk5250 \
     device/samsung/manta/ueventd.smdk5250.rc:root/ueventd.smdk5250.rc
 
+# Bootanimation
+PRODUCT_COPY_FILES += \
+    device/samsung/manta/bootanimation.zip:system/media/bootanimation.zip
+
+
 # Input device files for smdk5250
 PRODUCT_COPY_FILES += \
     device/samsung/manta/egalax_i2c.idc:system/usr/idc/egalax_i2c.idc \
@@ -176,7 +181,25 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=1024 \
-    ro.hwui.disable_scissor_opt=true
+    ro.hwui.disable_scissor_opt=true \
+    ro.carrier=wifi-only \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.feedback \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.wifi_required=true \
+    keyguard.no_require_sim=true \
+    ro.facelock.black_timeout=700 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=500 \
+    ro.facelock.use_intro_anim=true \
+    camera.flash_off=0 \
+    drm.service.enabled=true \
+    ro.com.widevine.cachesize=16777216
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
